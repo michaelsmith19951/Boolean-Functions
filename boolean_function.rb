6674a017ddeaf5e42fef1b1d4915fe3ab2e6719f
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-20 09:58:24 -0400

    Boolean function that checks to see if 2 numbers match

---
+++ boolean_function.rb
@@ -0,0 +1,15 @@
+# Function that checks if a number x is equal to a number y
+def variable(x, y)
+	if x == y then 
+		return true
+	else
+		return false
+	end
+end
+
+
+p variable(1, 2)
+p variable(2, 2)
+p variable(-1, 1)
+p variable(-5, -5)
+
